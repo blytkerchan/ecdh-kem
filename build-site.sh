6b91773878589274238d7bc5acf987e8177b4a59
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2023-07-21 14:18:11 -0400

    current state of the draft

--- build-site.sh
+++ build-site.sh
@@ -6,7 +6,7 @@ pip install -U pip
 pip install -Ur paper-requirements.txt
 jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace paper.ipynb
 jupyter nbconvert --execute --to notebook --inplace paper.ipynb
-jupyter nbconvert paper.ipynb --TagRemovePreprocessor.remove_cell_tags='html_only' --to latex --output-dir _site --output index
+jupyter nbconvert paper.ipynb --TagRemovePreprocessor.remove_cell_tags='html_only' --to pdf --output-dir _site --output index
 jupyter nbconvert paper.ipynb --TagRemovePreprocessor.remove_input_tags='html_only' --to html --template classic --output-dir _site --output index
-#ghp-import -n -p -f _site
+ghp-import -n -p -f _site
 jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace paper.ipynb
