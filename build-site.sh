e79a2365c476b5ea65ccb94f02db5db4617804e8
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2023-07-24 11:43:19 -0400

    This kinda sorta works
    I just copied the entire Jupyter template in, but I don't really like
    this "solution" because it doesn't seem particularly maintainable if I
    don't want to shadow Jupyter development (which I definitely don't)

--- build-site.sh
+++ build-site.sh
@@ -6,7 +6,7 @@ pip install -U pip
 pip install -Ur paper-requirements.txt
 jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace paper.ipynb
 jupyter nbconvert --execute --to notebook --inplace paper.ipynb
-jupyter nbconvert paper.ipynb --TagRemovePreprocessor.remove_cell_tags='html_only' --to pdf --output-dir _site --output index
-jupyter nbconvert paper.ipynb --TagRemovePreprocessor.remove_input_tags='html_only' --to html --template classic --output-dir _site --output index
-ghp-import -n -p -f _site
+jupyter nbconvert paper.ipynb --TagRemovePreprocessor.remove_cell_tags='html_only' --TagRemovePreprocessor.remove_input_tags='latex_only' --to pdf --template ./template --output-dir _site --output index
+jupyter nbconvert paper.ipynb --TagRemovePreprocessor.remove_input_tags='html_only' --TagRemovePreprocessor.remove_cell_tags='latex_only' --to html --template classic --output-dir _site --output index
+#ghp-import -n -p -f _site
 jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace paper.ipynb
